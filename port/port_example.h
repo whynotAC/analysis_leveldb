905246f54a105016cbb8dc423d4fb344e2d5f4ed
MTDP <mtdp@debug-Li.lan>
2019-12-28 23:41:30 +0800

    add process write

---
+++ port/port_example.h
@@ -0,0 +1,13 @@
+// This file contains the specification, but not the implementations,
+// of the types/operations/etc. that should be defined by a platform
+// specific port_<platform>.h file. Use this file as a reference for
+// how to port this package to a new platform
+
+#ifndef STORAGE_LEVELDB_PORT_PORT_EXAMPLE_H_
+#define STORAGE_LEVELDB_PORT_PORT_EXAMPLE_H_
+
+#include "port/thread_annotations.h"
+
+
+
+#endif
---
+++ util/mutexlock.h
@@ -0,0 +1,36 @@
+#ifndef STORAGE_LEVELDB_UTIL_MUTEXLOCK_H_
+#define STORAGE_LEVELDB_UTIL_MUTEXLOCK_H_
+
+#include "port/port.h"
+#include "port/thread_annotations.h"
+
+namespace leveldb {
+
+// Helper class that locks a mutex on construction and unlocks the mutex when
+// the destructor of the MutexLock object is invoked.
+//
+// Typical usage:
+//
+//  void MyClass::MyMethod() {
+//      MutexLock l(&mu_);      // mu_ is an instance variable
+//      ... some complex code, possibly with multiple return paths ...
+//   }
+
+class SCOPED_LOCKABLE MutexLock {
+  public:
+    explicit MutexLock(port::Mutex* mu) EXCLUSIVE_LOCK_FUNCTION(mu) : mu_(mu) {
+        this->mu_->Lock();
+    }
+    ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
+
+    MutexLock(const MutexLock&) = delete;
+    MutexLock& operator=(cosnt MutexLock&) = delete;
+
+  private:
+    port::Mutex* const mu_;
+};
+
+
+} // namespace leveldb
+
+#endif
