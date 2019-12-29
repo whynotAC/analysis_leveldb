0a23e31259a036523586af86c7a19f9e4eae6cae
MTDP <mtdp@debug-Li.lan>
2019-12-29 23:21:52 +0800

    add memtable

---
+++ db/memtable.cc
@@ -0,0 +1,17 @@
+#include "db/memtable.h"
+#include "db/dbformat.h"
+#include "leveldb/comparator.h"
+#include "leveldb/env.h"
+#include "leveldb/iterator.h"
+#include "util/coding.h"
+
+namespace leveldb {
+
+static Slice GetLengthPrefixedSlice(const char* data) {
+    uint32_t len;
+    const char* p = data;
+    p = GetVarint32Ptr(p, p + 5, &len); // +5: we assume "p" is not corrupetd
+    return Slice(p, len);
+}
+
+} // namespace leveldb
--- port/port_example.h
+++ port/port_example.h
@@ -8,6 +8,93 @@
 
 #include "port/thread_annotations.h"
 
+namespace leveldb {
+namespace port {
 
+// TODO(jorlow): Many of these belong more in the environment class rather than
+//               here. We should try moving them and see if it affects pref.
+
+// The following boolean constant must be true on a little-endian machine
+// and false otherwise.
+static const bool kLittleEndian = true; // or some other expression
+
+// -------------------------------thread-----------------------------
+
+// A Mutex represents an exclusive lock
+class LOCKABLE Mutex {
+  public:
+    Mutex();
+    ~Mutex();
+
+    // Lock the mutex. Waits until other lockers have exited.
+    // Will deadlock if the mutex is already locked by this thread.
+    void Lock() EXCLUSIVE_LOCK_FUNCTION();
+
+    // Unlock the mutex.
+    // REQUIRES: This mutex was locked by this thread.
+    void Unlock() UNLOCK_FUNCTION();
+
+    // Optionally crash if this thread does not hold this mutex.
+    // The implementation must be fast, especially if NDEBUG is
+    // defined. The implementation is allowed to skip all checks.
+    void AssertHeld() ASSERT_EXCLUSIVE_LOCK();
+};
+
+class CondVar {
+  public:
+    explicit CondVar(Mutex* mu);
+    ~CondVar();
+
+    // Atomically release *mu and block on this condition variable until
+    // either a call to SignalAll(), or a call to Signal() that picks
+    // this thread to wakeup.
+    // REQURIES: this thread holds *mu
+    void Wait();
+
+    // If these are some threads waiting, wake up at least one of them.
+    void Signal();
+
+    // Wake up all waiting threads.
+    void SignalAll();
+};
+
+// ------------------------------Compression------------------------
+
+// Store the snappy compression of "input[0,input_length-1]" in *output
+// Returns false if snappy is not supported by this port
+bool Snappy_Compress(const cahr* input, size_t input_length,
+                        std::string* output);
+
+// If input[0,input_length-1] looks like a valid snappy compressed
+// buffer, store the size of the uncompressed data in *result and
+// return true. Else return false
+bool Snappy_GetUncompressedLength(const char* input, size_t length,
+                                    size_t* result);
+
+// Attempt to snappy uncompress input[0,input_length-1] into *output
+// Returns true if successful, false if the input is invalid lightweight
+// compressed data.
+//
+// REQUIRES: at least the first "n" bytes of output[] must be writable
+// where "n" is the result of a successful call to
+// Snappy_GetUncompressedLegnth
+bool Snappy_Uncompress(const char* input_data, size_t input_length,
+                        char* output);
+
+// -----------------------------Miscellaneous----------------------
+
+// If heap profiling is not supported, returns false.
+// Else  repeatedly calls (*func)(arg, data, n) and them returns true.
+// The concatenation of all "data[0,n-1]" fragments is the heap profile.
+bool GetHeapProfile(void (*func)(void*, const char*, int), void* arg);
+
+// Extend the CRC to include the first n bytes of buf.
+//
+// Returns zero if the CRC cannot be extended using acceleration, else returns
+// the newly extended CRC valud (which may also be zero)
+uint32_t AcceleratedCRC32(uint32_t crc, const char* buf, size_t size);
+
+} // namespace port
+} // namespace leveldb
 
 #endif
