6cc129cc45d2f24f83fa32dafb97bd29e6d76211
MTDP <mtdp@debug-Li.lan>
2020-05-31 20:03:05 +0800

    restart

---
+++ db/builder.h
@@ -0,0 +1,26 @@
+#ifndef STORAGE_LEVELDB_DB_BUILDER_H_
+#define STORAGE_LEVELDB_DB_BUILDER_H_
+
+#include "leveldb/status.h"
+
+namespace leveldb {
+
+struct Options;
+struct FileMetaData;
+
+class Env;
+class Iterator;
+class TableCache;
+class VersionEdit;
+
+// Build a Table file from the contents of *iter. The generated fiile
+// will be named according to meta->number. On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname, Env* env, const Options& options,
+                    TableCache* table_cache, Iterator* iter, FileMetaData* meta);
+
+}
+
+#endif
